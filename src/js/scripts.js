4db4d5043a0cff12c0477db205634f50b86ab507
Olga-fd <olanef9@gmail.com>
2022-08-17 00:10:28 +0500

    changed swiperSecond

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,22 @@
+"use strict";
+
 function setWidth() {
   const drag = document.querySelector('.swiper-scrollbar-drag');
   const active = document.querySelector('.secondSwiper .swiper-pagination-bullet-active');
+  const block1 = document.querySelector('.works1');
+  const block2 = document.querySelector('.works2');
   let width = getComputedStyle(active).width;
   drag.style.width = width;
 
   if (active.innerHTML == 'Услуги по ретуши') {
     drag.style.transform = 'translate3d(283.5px, 0px, 0px)';
+    block2.classList.remove('unvisible');
+    block1.classList.add('unvisible');
+
   } else {
     drag.style.transform = 'translate3d(0px, 0px, 0px)';
+    block1.classList.remove('unvisible');
+    block2.classList.add('unvisible');
   }
 }
 
@@ -20,7 +29,5 @@ document.querySelector('.secondSwiper .swiper-scrollbar').addEventListener('clic
   setWidth();
 })
 
-// document.querySelector('.secondSwiper .content2').addEventListener('slide', () => {
-//   setWidth();
-// })
+
 
